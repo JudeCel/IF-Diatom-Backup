8b3e275ae6d23c0cba1ab0776f067a85f4573d19
Takhir Mamirov <takhirm@diatomenterprises.com>
2014-11-25 14:09:55 +0200

    Clean up.

--- server/handlers/getUser.js
+++ server/handlers/getUser.js
@@ -4,35 +4,16 @@ var webFaultHelper = require('../helpers/webFaultHelper.js');
 var joi = require('joi');
 
 module.exports.validate = function (req, res, next) {
-  
-    var err = joi.validate(req.query, {
-        user_id: joi.types.Number().required()
-    });
-    if (err)
-        return next(webFaultHelper.getValidationFault(err.message));
+    if (req.locals.userId == undefined)
+        return next(webFaultHelper.getValidationFault("UserId is missed"));
 
     next();
 };
 
 module.exports.run = function (req, resCb, errCb) {
-    getUser(req.query)
+    getUser(req.locals)
         .done(function (data) {
-            var user = {};
-            user.id = data.id;
-            user.name_first  =  data.name_first;
-            user.name_last  =  data.name_last;
-            user.gender = data.gender;
-            user.email  = data.email;
-            user.address  = data.address;
-            user.phone  = data.phone;
-            user.mobile  = data.mobile;
-            user.state = data.state;
-            user.country_id = data.country_id;
-            user.city = data.city;
-            user.code = data.code;
-            user.company = data.company;
-
-            resCb.send(user);
+            resCb.send(data);
         }, function (err) {
             errCb(webFaultHelper.getFault(err));
         });
